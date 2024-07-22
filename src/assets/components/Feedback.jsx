0360b0b7267894b66544138a9c179a8504365a51
1Rendes <1admin@ukr.net>
2024-07-22 17:27:16 +0200

    rev 1.0

---
+++ src/assets/components/Feedback.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+const Feedback = ({ feedback, goodFeedBackPart, totalFeedBack }) => {
+  return (
+    <div>
+      <p>Goog: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedBack}</p>
+      <p>PositiveFeedback: {goodFeedBackPart}</p>
+    </div>
+  );
+};
+
+export default Feedback;
---
+++ src/assets/components/Options.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+const Options = ({ onUpdate, totalFeedBack }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          onUpdate("good");
+        }}
+      >
+        Goog
+      </button>
+      <button
+        onClick={() => {
+          onUpdate("neutral");
+        }}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          onUpdate("bad");
+        }}
+      >
+        Bad
+      </button>
+
+      {totalFeedBack ? (
+        <button
+          onClick={() => {
+            onUpdate("reset");
+          }}
+        >
+          Reset
+        </button>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Options;
